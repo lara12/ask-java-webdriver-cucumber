11800e569a1cfd81abf81f37ff38508755d391e9
ASYStent <dovgichnik@gmail.com>
2018-08-03 12:41:29 -0700

    [CHANGED] Scennarios with 'Copy' and 'Cut' for password was commented

--- src/test/java/support/TestRunner.java
+++ src/test/java/support/TestRunner.java
@@ -8,7 +8,8 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         plugin = {"pretty", "html:target/cucumber", "json:target/cucumber/report.json"},
         features = "src/test/resources/features",
-        glue = {"definitions", "support"}
+        glue = {"definitions", "support"},
+        tags = "@ACA-490"
 )
 public class TestRunner {
 }
