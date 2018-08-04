de7050af17c27d1a001cb4bd3c176411f2e0a0e3
ASYStent <dovgichnik@gmail.com>
2018-08-03 17:25:02 -0700

    [DELETED] Tag '@Test_Objects/properties/methods' in TestRunner (repeat)

--- src/test/java/support/TestRunner.java
+++ src/test/java/support/TestRunner.java
@@ -8,8 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         plugin = {"pretty", "html:target/cucumber", "json:target/cucumber/report.json"},
         features = "src/test/resources/features",
-        glue = {"definitions", "support"},
-        tags = "@Test_Objects/properties/methods"
+        glue = {"definitions", "support"}
 )
 public class TestRunner {
 }
